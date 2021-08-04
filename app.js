aab1edd125fba741ad8fcc4ab5544cef39df6ce0
mind0bender <mind0bender@gmail.com>
2021-08-05 00:32:16 +0530

    terminal link & spacing & some minor changes

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const cors = require("cors");
 const morgan = require("morgan");
 const chalk = require("chalk");
 const socket = require("socket.io");
+const link = require("./terminallink");
 
 const PORT = process.env.PORT || 8000;
+const ISDEV = process.env.NODE_ENV !== "production";
 const app = express();
 
 let blue = 0;
@@ -15,16 +17,30 @@ app.use(express.static("public"));
 
 const server = app.listen(PORT, () => {
   console.clear();
-  console.log(chalk.cyanBright(`Server started on PORT ${PORT} at ${Date()}`));
+  console.log(
+    chalk.cyanBright(
+      `Server started on PORT ${`${PORT} ${
+        ISDEV ? `http://localhost:${PORT}` : ""
+      }`} at ${Date()}`
+    )
+  );
 });
 
 const io = socket(server);
 
 io.sockets.on("connection", (soc) => {
   soc.emit("blue", blue);
-  console.log(`joined    ${soc.id}`);
+  console.log(
+    chalk.greenBright(
+      `___________________________________\n|joined-------${soc.id}|\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n`
+    )
+  );
   soc.on("disconnect", () => {
-    console.log(`left      ${soc.id}`);
+    console.log(
+      chalk.redBright(
+        `____________________________________\n|left---------${soc.id}|\n‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n`
+      )
+    );
   });
   soc.on("getblue", () => {
     soc.emit("blue", blue);
@@ -32,8 +48,9 @@ io.sockets.on("connection", (soc) => {
   soc.on("postblue", (data) => {
     blue = data;
     console.log(
-      chalk.blueBright(`blue ${blue}`),
-      chalk.whiteBright(`<<- ${soc.id}`)
+      chalk.bgRgb(0, 0, blue)(blue),
+      (String(data).length == 1 ? "  " : String(data).length == 2 ? " " : "") +
+        chalk.whiteBright(`<<-  ${soc.id}`)
     );
     soc.broadcast.emit("blue", blue);
   });
