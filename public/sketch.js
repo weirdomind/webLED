88948f7e2f0cf76e14406cb76d200cf76d3fa55b
mind0bender <mind0bender@gmail.com>
2021-08-04 19:55:27 +0530

    sample div

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,19 @@
 const socket = io.connect();
 const blueslider = document.getElementById("blueslider");
+const sample = document.getElementById("sample");
 let blue = 0;
 
 socket.on("blue", (data) => {
   blue = data;
   blueslider.value = data;
+  sample.style.backgroundColor = `rgb(${255 - data}, ${255 - data}, ${255})`;
   console.log("got blue", data);
 });
 
 blueslider.oninput = (e) => {
   const data = e.target.value;
   blue = data;
+  sample.style.backgroundColor = `rgb(${255 - data}, ${255 - data}, ${255})`;
   socket.emit("postblue", data);
   console.log(data);
 };
