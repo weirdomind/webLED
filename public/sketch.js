71ab9fbe1d3b1f61911b2945ce0b6775333d1f2a
mind0bender <mind0bender@gmail.com>
2021-08-07 15:43:34 +0530

    RGB single anode implemented

--- public/sketch.js
+++ public/sketch.js
@@ -11,12 +11,3 @@ colorinp.oninput = (e) => {
   socket.emit("forward", { eventname: "postcolor", data: e.target.value });
   console.log(e.target.value);
 };
-
-function rgb2hex(rgb) {
-  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
-
-function hex(x) {
-  return isNaN(x) ? "00" : hexDigits[(x - (x % 16)) / 16] + hexDigits[x % 16];
-}
